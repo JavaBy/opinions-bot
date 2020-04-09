f90034058920263e1d729c4600aff1df3104e81f
madhead <siarhei.krukau@gmail.com>
2020-04-09 03:00:15 +0300

    Pre-warm DynamoDB connection

--- src/main/kotlin/by/jprof/telegram/opinions/config/dynamo.kt
+++ src/main/kotlin/by/jprof/telegram/opinions/config/dynamo.kt
@@ -1,13 +1,22 @@
 package by.jprof.telegram.opinions.config
 
 import by.jprof.telegram.opinions.dao.VotesDAO
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.future.await
+import kotlinx.coroutines.launch
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
 
 val dynamoModule = module {
     single {
-        DynamoDbAsyncClient.builder().build()
+        val client = DynamoDbAsyncClient.builder().build()
+
+        GlobalScope.launch {
+            client.describeEndpoints().await()
+        }
+
+        client
     }
 
     single {
