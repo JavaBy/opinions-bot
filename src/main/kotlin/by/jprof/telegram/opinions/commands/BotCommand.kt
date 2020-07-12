7d2ea856a84559ce15d57db61adf2459ca7cfd15
Waldemar Tsiamruk <wtsiamruk@gmail.com>
2020-07-12 23:23:18 +0300

    Remove commands

--- src/main/kotlin/by/jprof/telegram/opinions/commands/BotCommand.kt
+++
@@ -1,5 +0,0 @@
-package by.jprof.telegram.opinions.commands
-
-interface BotCommand {
-    fun execute(chatIdentifier: Long, vararg parameters: String)
-}
--- src/main/kotlin/by/jprof/telegram/opinions/commands/commandNames.kt
+++
@@ -1,4 +0,0 @@
-package by.jprof.telegram.opinions.commands
-
-const val ADD_TO_WHITE_LIST = "/add_to_whitelist"
-const val REMOVE_FROM_WHITE_LIST = "/remove_from_whitelist"
