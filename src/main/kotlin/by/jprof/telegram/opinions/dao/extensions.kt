eda7c25ef1b6d8781db285f55d71064e00e04cba
slesh <slesh.eugene93@gmail.com>
2020-08-16 14:21:05 +0300

    asText utility extension function

--- src/main/kotlin/by/jprof/telegram/opinions/dao/extensions.kt
+++ src/main/kotlin/by/jprof/telegram/opinions/dao/extensions.kt
@@ -1,6 +1,10 @@
 package by.jprof.telegram.opinions.dao
 
+import com.github.insanusmokrassar.TelegramBotAPI.types.message.CommonMessageImpl
+import com.github.insanusmokrassar.TelegramBotAPI.types.message.content.TextContent
+import com.github.insanusmokrassar.TelegramBotAPI.types.update.MessageUpdate
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+import com.github.insanusmokrassar.TelegramBotAPI.types.update.abstracts.Update
 
 fun String.toAttributeValue(): AttributeValue = AttributeValue.builder().s(this).build()
 
@@ -10,4 +14,10 @@ fun Map<String, AttributeValue>.toAttributeValue() = AttributeValue.builder().m(
 
 fun Map<String, AttributeValue>.require(attr: String, message: String? = null): AttributeValue {
     return this[attr] ?: throw IllegalStateException(message ?: "Missing '$attr' attribute!")
+}
+
+fun Update.asText(): TextContent? {
+    val message = (this as? MessageUpdate) ?: return null
+    val content = (message.data as? CommonMessageImpl<*>) ?: return null
+    return content.content as? TextContent
 }
--- src/main/kotlin/by/jprof/telegram/opinions/processors/CommandProcessor.kt
+++ src/main/kotlin/by/jprof/telegram/opinions/processors/CommandProcessor.kt
@@ -1,8 +1,6 @@
 package by.jprof.telegram.opinions.processors
 
-import com.github.insanusmokrassar.TelegramBotAPI.types.message.CommonMessageImpl
-import com.github.insanusmokrassar.TelegramBotAPI.types.message.content.TextContent
-import com.github.insanusmokrassar.TelegramBotAPI.types.update.MessageUpdate
+import by.jprof.telegram.opinions.dao.asText
 import com.github.insanusmokrassar.TelegramBotAPI.types.update.abstracts.Update
 
 abstract class CommandProcessor(
@@ -16,11 +14,9 @@ abstract class CommandProcessor(
 
     abstract suspend fun doProcess(update: Update)
 
-    protected fun isCommand(update: Update): Boolean {
-        val message = (update as? MessageUpdate) ?: return false
-        val content = (message.data as? CommonMessageImpl<*>) ?: return false
-        val text = (content.content as? TextContent) ?: return false
-
-        return (text.text.startsWith("/") && text.text.drop(1).split(" ")[0].split("@")[0] == cmd)
+    private fun isCommand(update: Update): Boolean {
+        val text = update.asText() ?: return false
+        return (text.text.startsWith("/") &&
+                text.text.drop(1).split(" ")[0].split("@")[0] == cmd)
     }
 }
