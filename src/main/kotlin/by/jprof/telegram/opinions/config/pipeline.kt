c990c85f4448dcd7b2d932737343ad02b19d63a8
slesh <slesh.eugene93@gmail.com>
2020-08-22 12:57:13 +0300

    Declared Tesseract bean

--- src/main/kotlin/by/jprof/telegram/opinions/config/pipeline.kt
+++ src/main/kotlin/by/jprof/telegram/opinions/config/pipeline.kt
@@ -1,5 +1,6 @@
 package by.jprof.telegram.opinions.config
 
+import by.jprof.telegram.opinions.processors.Tesseract
 import by.jprof.telegram.opinions.processors.JEPLinksProcessor
 import by.jprof.telegram.opinions.processors.KotlinMentionsProcessor
 import by.jprof.telegram.opinions.processors.KotlinStatsCommandProcessor
@@ -8,6 +9,7 @@ import by.jprof.telegram.opinions.processors.UpdateProcessor
 import by.jprof.telegram.opinions.processors.YoutubeLinksProcessor
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import java.io.File
 
 val pipelineModule = module {
     single {
@@ -28,5 +30,9 @@ val pipelineModule = module {
 
     single<UpdateProcessor>(named("KotlinStatsCommandProcessor")) {
         KotlinStatsCommandProcessor(get(), get())
+    }
+
+    single {
+        Tesseract()
     }
 }
